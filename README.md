1ac6b94d66363c6f05720c3692b0174c88abcc81
barkha-gupta <barkhagupta401@gmail.com>
2023-11-05 15:19:18 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# laundry-cart
+Laundry cart is a small MERN stack project.
+
+Here is the link to what I am making: https://xd.adobe.com/view/f53d6af6-d6ff-4854-b16e-8f2fd1c520cb-a06e/
---
+++ client/src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+        <span>2022</span>
+        <span>
+            <i className="fa-regular fa-copyright"></i>
+        </span>
+        <span>Laundry</span>
+    </footer>
+  )
+}
